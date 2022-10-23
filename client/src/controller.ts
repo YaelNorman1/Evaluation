24cba09ce870d81e924b16d461fb7932c5ed3bde
YaelNorman1 <YaelNorman2808@gmail.com>
2022-10-23 12:26:16 +0300

    end point and filter in server are working

--- client/src/controller.ts
+++ client/src/controller.ts
@@ -1,1 +1,8 @@
-console.log("hello111")
+
+
+$("#submitIngredient").on("click", async function (){
+    const dataFromUser= get_data_from_input();
+    await playersDataModule.generateNewTeamPlayers(dataFromUser);
+    let players= getPlayersIfBirthDate();
+    render.renderPlayersToScreen(players)
+})
